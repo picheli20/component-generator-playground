5e72d96152246eadc73f2c14884d90f762bfc0b1
Fabio Picheli <contato@fabiopicheli.com>
2018-07-05 18:07:53 +0200

    feat: adding hello world

--- my-component/src/my-component/index.ts
+++ my-component/src/my-component/index.ts
@@ -5,6 +5,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // per file.
 export function myComponent(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.create(options.name || 'hello', 'world');
     return tree;
   };
 }
